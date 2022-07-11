0e3a222e7bd17423be6ee31b849145cc66bddb48
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-07-10 20:12:55 -0500

    Parameterizes normal item sell in tests.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,30 +1,29 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class GildedRoseTest {
-    @Test
-    void updateQuality_normalItem_sellInDecrementsBy1_whenPositive() {
-        Item[] items = new Item[] { new Item("normal item", 1, 1) };
+    @ParameterizedTest(name = "updateQuality_normalItem_decrementsBy1Always_startingSellInIs{0}_updatedSellInIs{1}")
+    @MethodSource
+    void updateQuality_normalItem_sellInAlwaysDecrementsBy1(int sellIn, int expected) {
+        Item[] items = new Item[] { new Item("normal item", sellIn, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertThat(app.items[0].sellIn).isZero();
+        assertThat(app.items[0].sellIn).isEqualTo(expected);
     }
-    @Test
-    void updateQuality_normalItem_sellInDecrementsBy1_when0() {
-        Item[] items = new Item[] { new Item("normal item", 0, 1) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].sellIn).isEqualTo(-1);
-    }
-    @Test
-    void updateQuality_normalItem_sellInDecrementsBy1_whenNegative() {
-        Item[] items = new Item[] { new Item("normal item", -1, 1) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].sellIn).isEqualTo(-2);
+    private static Stream<Arguments> updateQuality_normalItem_sellInAlwaysDecrementsBy1() {
+        return Stream.of(
+                Arguments.of(1, 0), // not expired
+                Arguments.of(0, -1), // expires today
+                Arguments.of(-1, -2) // expired
+        );
     }
     @Test
     void updateQuality_normalItem_qualityDecrementsBy1_whenSellInIsPositive() {
