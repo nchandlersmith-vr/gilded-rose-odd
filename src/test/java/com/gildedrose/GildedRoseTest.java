9916e5e4863a2b68a32289f31ae3955be4d21df1
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-07-10 19:40:04 -0500

    Adds quality tests for a normal item

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -20,10 +20,52 @@ class GildedRoseTest {
         assertThat(app.items[0].sellIn).isEqualTo(-1);
     }
     @Test
-    void updateQuality_normalItem_sellIn_DecrementsBy1_whenNegative() {
+    void updateQuality_normalItem_sellInDecrementsBy1_whenNegative() {
         Item[] items = new Item[] { new Item("normal item", -1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertThat(app.items[0].sellIn).isEqualTo(-2);
+    }
+    @Test
+    void updateQuality_normalItem_qualityDecrementsBy1_whenSellInIsPositive() {
+        Item[] items = new Item[] { new Item("normal item", 1, 1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isZero();
+    }
+    @Test
+    void updateQuality_normalItem_qualityDecrementsBy1_whenSellInIsZero() {
+        Item[] items = new Item[] { new Item("normal item", 0, 1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isZero();
+    }
+    @Test
+    void updateQuality_normalItem_qualityDecrementsBy2_whenSellInIsNegative() {
+        Item[] items = new Item[] { new Item("normal item", -1, 2)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isZero();
+    }
+    @Test
+    void updateQuality_normalItem_qualityCannotBeLessThanZero_whenSellInIsPositive() {
+        Item[] items = new Item[] { new Item("normal item", 1, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isZero();
+    }
+    @Test
+    void updateQuality_normalItem_qualityCannotBeLessThanZero_whenSellInIsZero() {
+        Item[] items = new Item[] { new Item("normal item", 0,0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isZero();
+    }
+    @Test
+    void updateQality_normalItem_qualityCannotBeLessThanZero_whenSellInIsNegative() {
+        Item[] items = new Item[] { new Item("normal item", -1, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isZero();
     }
 }
