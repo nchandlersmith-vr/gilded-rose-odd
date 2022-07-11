a7507e42ea003c0b7fe9403465ec2f474987b13d
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-07-11 17:56:26 -0500

    Refactors additional normal item tests into parameterized tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import com.sun.org.apache.xpath.internal.Arg;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -10,40 +11,33 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class GildedRoseTest {
-    @ParameterizedTest(name = "updateQuality_normalItem_decrementsBy1Always_startingSellInIs{0}_updatedSellInIs{1}")
-    @MethodSource("updateQuality_normalItem_sellInAlwaysDecrementsBy1_argsOf_startingSellIn_endingSellIn")
+    @ParameterizedTest(name = "updateQuality_normalItem_decrementsBy1Always_startingSellIn{0}_updatedSellIn{1}")
+    @MethodSource
     void updateQuality_normalItem_sellInAlwaysDecrementsBy1(int startingSellIn, int endingSellIn) {
         Item[] items = new Item[] { new Item("normal item", startingSellIn, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertThat(app.items[0].sellIn).isEqualTo(endingSellIn);
     }
-    private static Stream<Arguments> updateQuality_normalItem_sellInAlwaysDecrementsBy1_argsOf_startingSellIn_endingSellIn() {
+    private static Stream<Arguments> updateQuality_normalItem_sellInAlwaysDecrementsBy1() {
         Arguments notExpired = Arguments.of(1, 0);
         Arguments expiresToday = Arguments.of(0, -1);
         Arguments expired = Arguments.of(-1, -2);
         return Stream.of(notExpired, expiresToday, expired);
     }
-    @Test
-    void updateQuality_normalItem_qualityDecrementsBy1_whenSellInIsPositive() {
-        Item[] items = new Item[] { new Item("normal item", 1, 1)};
+    @ParameterizedTest
+    @MethodSource
+    void updateQuality_normalItem_qualityDegrades(int sellIn, int startingQuality, int endingQuality) {
+        Item[] items = new Item[] { new Item("normal item", sellIn, startingQuality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertThat(app.items[0].quality).isZero();
+        assertThat(app.items[0].quality).isEqualTo(endingQuality);
     }
-    @Test
-    void updateQuality_normalItem_qualityDecrementsBy1_whenSellInIsZero() {
-        Item[] items = new Item[] { new Item("normal item", 0, 1)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].quality).isZero();
-    }
-    @Test
-    void updateQuality_normalItem_qualityDecrementsBy2_whenSellInIsNegative() {
-        Item[] items = new Item[] { new Item("normal item", -1, 2)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].quality).isZero();
+    private static Stream<Arguments> updateQuality_normalItem_qualityDegrades() {
+        Arguments notExpiredLoses1QualityPoint = Arguments.of(1, 1 , 0);
+        Arguments expiresTodayLoses1QualityPoint = Arguments.of(0, 1 , 0);
+        Arguments expiredLoses2QualityPoints = Arguments.of(-1, 2 , 0);
+        return Stream.of(notExpiredLoses1QualityPoint, expiresTodayLoses1QualityPoint, expiredLoses2QualityPoints);
     }
     @Test
     void updateQuality_normalItem_qualityCannotBeLessThanZero_whenSellInIsPositive() {
