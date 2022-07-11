856385e8f894b735f5812f46fded1a78bd12a6dd
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-07-10 19:14:07 -0500

    Refactors

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,21 +7,21 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class GildedRoseTest {
     @Test
     void updateQuality_normalItem_sellInDecrementsBy1_whenPositive() {
-        Item[] items = new Item[] { new Item("foo", 1, 1) };
+        Item[] items = new Item[] { new Item("normal item", 1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertThat(app.items[0].sellIn).isEqualTo(0);
+        assertThat(app.items[0].sellIn).isZero();
     }
     @Test
     void updateQuality_normalItem_sellInDecrementsBy1_when0() {
-        Item[] items = new Item[] { new Item("foo", 0, 1) };
+        Item[] items = new Item[] { new Item("normal item", 0, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertThat(app.items[0].sellIn).isEqualTo(-1);
     }
     @Test
     void updateQuality_normalItem_sellIn_DecrementsBy1_whenNegative() {
-        Item[] items = new Item[] { new Item("foo", -1, 1) };
+        Item[] items = new Item[] { new Item("normal item", -1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertThat(app.items[0].sellIn).isEqualTo(-2);
