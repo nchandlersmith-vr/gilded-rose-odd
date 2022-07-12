9c09290a8d20a9f6f6696bcf8cf76ed83aa1e671
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-07-12 10:27:54 -0500

    Refactors experation rules and sellin decrement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -38,29 +38,35 @@ class GildedRose {
                     }
                 }
             }
+            decrementSellIn(i);
+            enforceExperationRules(i);
+            System.out.printf("Resulting item: %s.%n", items[i].toString());
+        }
+    }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+    private void enforceExperationRules(int i) {
+        if (items[i].sellIn < 0) {
+            if (!items[i].name.equals("Aged Brie")) {
+                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality = items[i].quality - 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    items[i].quality = items[i].quality - items[i].quality;
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
                 }
             }
-            System.out.printf("Resulting item: %s.%n", items[i].toString());
+        }
+    }
+
+    private void decrementSellIn(int i) {
+        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[i].sellIn = items[i].sellIn - 1;
         }
     }
 }
